02479f4e20a324a84117a46b7f9a07cbf3f9554d
Cu63 <scube.str@gmail.com>
2022-06-20 01:20:56 +0300

    working model

--- ex1/arima.py
+++ ex1/arima.py
@@ -3,6 +3,7 @@ import statsmodels.api as sm
 from statsmodels.iolib.table import SimpleTable
 from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
+import itertools
 import numpy as np
 
 def calc_d_coef(data):
@@ -10,7 +11,7 @@ def calc_d_coef(data):
     print ('adf: ', test[0])
     print ('p-value: ', test[1])
     print('Critical values: ', test[4])
-    if (test[0]> test[4]['5%']):
+    if (test[1] >= 0.05):
         print ('есть единичные корни, ряд не стационарен')
     else:
         print ('единичных корней нет, ряд стационарен')
@@ -23,36 +24,36 @@ def calc_d_coef(data):
         print ('adf: ', test[0])
         print ('p-value: ', test[1])
         print('Critical values: ', test[4])
-        if (test[0]> test[4]['5%']):
+        if (test[1] >= 0.05):
             print ('есть единичные корни, ряд не стационарен')
+            d += 1
         else:
             print ('единичных корней нет, ряд стационарен')
             break
     return d
 
 def calc_q_p_coef(data, d):
+    lags_count = 25 
     data_diff = data.diff(periods=d).dropna()
     fig = plt.figure()
     data_diff.plot(figsize=(12,6))
     ig = plt.figure(figsize=(12,8))
     ax1 = ig.add_subplot(211)
-    fig = sm.graphics.tsa.plot_acf(data_diff.values.squeeze(), lags=25, ax=ax1)
+    fig = sm.graphics.tsa.plot_acf(data_diff.values.squeeze(), lags=lags_count, ax=ax1)
     ax2 = fig.add_subplot(212)
-    fig = sm.graphics.tsa.plot_pacf(data_diff, lags=25, ax=ax2)
+    fig = sm.graphics.tsa.plot_pacf(data_diff, lags=lags_count, ax=ax2, method='ywm')
 
-    acf = sm.tsa.acf(data_diff.values.squeeze(), nlags=25)
-    pacf = sm.tsa.pacf(data_diff.values.squeeze(), nlags=25)
-    q = sum(abs(x) >= 0.25 for x in acf)
-    p = sum(abs(x) >= 0.25 for x in pacf)
-
+    acf = sm.tsa.acf(data_diff.values.squeeze(), nlags=lags_count)
+    pacf = sm.tsa.pacf(data_diff.values.squeeze(), nlags=lags_count)
+    p = sum(abs(x) >= 0.20 for x in acf)
+    q = sum(abs(x) >= 0.20 for x in pacf)
     return q, p
 
 
 def read_data_from_file(fileName):
     dataset = read_csv(fileName, sep = ',', index_col=['date'], parse_dates=['date'], dayfirst=True)
     data = dataset.data
-    #resample data for weekends
-    #data = data.resample('W').sum()
+    data = data.resample('15T').sum()
     create_plot(data)
     return data
 
@@ -60,52 +61,24 @@ def create_plot(d):
     fig = plt.figure()
     d.plot(figsize = (12, 6))
 
-def analys(data):
-    itog = data.describe()
-    #coefficient of variation
-    print('V = %f' % (itog['std']/itog['mean']))
-    row =  [u'JB', u'p-value', u'skew', u'kurtosis']
-    jb_test = sm.stats.stattools.jarque_bera(data)
-    a = np.vstack([jb_test])
-    itog = SimpleTable(a, row)
-    print(itog)
-
-    data_diff = data.diff(periods=1).dropna()
-    test = sm.tsa.adfuller(data_diff)
-    print ('adf: ', test[0])
-    print ('p-value: ', test[1])
-    print('Critical values: ', test[4])
-    if (test[0]> test[4]['5%']):
-        print ('есть единичные корни, ряд не стационарен')
-    else:
-        print ('единичных корней нет, ряд стационарен')
-    """
-    #сравнения мат ожидания в для разных промежутков
-    m = data_diff.index[int(len(data_diff.index)/2+1)]
-    r1 = sm.stats.DescrStatsW(data_diff[m:])
-    r2 = sm.stats.DescrStatsW(data_diff[:m])
-    print('p-value: ', sm.stats.CompareMeans(r1,r2).ttest_ind()[1])
-    create_plot(data_diff)
-    """
-
-def model(data, d, q, p):
+def model(data, p, d, q):
     end = len(data)
-    dot = int(end  * 0.9)
+    dot = int(end * 0.95)
     src_data_model = data[:dot]
-    print(len(src_data_model))
-    #model = sm.tsa.ARIMA(src_data_model, order=(d, q, p)""", freq='W'""").fit()
-    model = sm.tsa.ARIMA(src_data_model, order=(d, q, p)).fit()
-#    print(model.summary())
-    pred = model.predict(dot, end, typ='levels')
-    trn = data[dot:]
-    print("pred len" + str(len(pred)))
-    print("trn len" + str(len(trn)))
-    r2 = r2_score(trn, pred[:len(trn)])
-    print("ARIMA(%d, %d, %d) %1.2f" % (p, d, q, r2))
-#    print("ARIMA(" + str(p) + ", " + str(d) + ", " + str(q) + ")" + '%1.2f' % r2)
-#    create_plot(data)
-#    pred.plot(style='r--')
-#    print(pred)
+    model = sm.tsa.ARIMA(data, freq='15T', order=(p, d, q)).fit()
+
+    pred = model.predict(dot, end + 10, typ='levels', dynamic = True)
+    diff=np.subtract(data[dot:],pred[:len(data)])
+    square=np.square(diff)
+    MSE=square.mean()
+    RMSE=np.sqrt(MSE)
+    mape = np.mean(np.abs((data[dot:] - pred[:len(data)])/data[dot:]))*100
+    print("ARIMA(%d, %d, %d) %1.2f, mape = %f" % (p, d, q, RMSE, mape))
+    q_test = sm.tsa.stattools.acf(model.resid, qstat=True) #свойство resid, хранит остатки модели, qstat=True, означает что применяем указынный тест к коэф-ам
+    print(DataFrame({'Q-stat':q_test[1], 'p-value':q_test[2]}))
+    create_plot(data)
+    pred.plot(style='r--', label = "ARIMA(%d, %d, %d)" % (p,d,q))
+    plt.legend()
     return pred
 
 
@@ -115,19 +88,51 @@ d = 0
 q = 0
 p = 0
 predicts = []
-data = read_data_from_file('24-hours1.csv')
+data = read_data_from_file('96-hours.csv')
+#data = read_data_from_file('24-hours.csv')
+#data = read_data_from_file('48-hours.csv')
 d = calc_d_coef(data)
 q, p = calc_q_p_coef(data, d)
-
+print("p = %d, d = %d, q = %d\n" % (p, d, q))
 predicts.append(model(data, p, d, q))
-predicts.append(model(data, p, d+1, q))
-predicts.append(model(data, p, d-1, q-1))
-predicts.append(model(data, p+2, d, q+3))
-labeles = ["ARIMA(p, d, q)", "ARIMA(p, d + 1, q)", "ARIMA(p, d-1, q-1)",
-        "ARIMA(p+2, d, q+3)"]
+predicts.append(model(data, 0, 1, 2))
+labeles = ["ARIMA(%d, %d, %d)" % (p,d,q), "ARIMA(0, 1, 2)"]
 create_plot(data)
+mid_pred = []
 for i in range(len(predicts)):
     predicts[i].plot(label = labeles[i])
+plt.legend()
+
+mean_pred = predicts[0]
+for i in range(len(mean_pred)):
+    mean_pred[i] = (mean_pred[i] + predicts[1][i]) / 2
+
+create_plot(data)
+mean_pred.plot(label = "mean predictions", style = 'r--')
+plt.legend()
+
+dot = int(len(data) / 2)
+diff=np.subtract(data[dot:],mean_pred[:len(data)])
+square=np.square(diff)
+MSE=square.mean()
+RMSE=np.sqrt(MSE)
+mape = np.mean(np.abs((data[dot:] - mean_pred[:len(data)])/data[dot:]))*100
+print("Mean %1.2f, mape = %f" % (RMSE, mape))
+
 if (graph == True):
     plt.legend()
     plt.show()
+"""
+p = range(0,10)
+d = q = range(0,3)
+pdq = list(itertools.product(p, d, q))
+best_pdq = (0,0,0)
+best_aic = np.inf
+for params in pdq:
+  model_test = sm.tsa.ARIMA(data, order = params)
+  result_test = model_test.fit()
+  if result_test.aic < best_aic:
+    best_pdq = params
+    best_aic = result_test.aic
+print(best_pdq, best_aic)
+"""
